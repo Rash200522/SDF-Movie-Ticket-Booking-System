472e269bb40b75badecdb83bb021902cc66fa799
hasangihaz <hasangihaz311@gmail.com>
2026-07-22 20:17:36 +0530

    Updated member1 data functions

--- member1_data.c
+++ member1_data.c
@@ -13,41 +13,66 @@ void initializeData() {
     showtimes[0].bookingCount = 0;
     showtimes[0].totalRevenue = 0;
     showtimes[0].totalTicketsSold = 0;
-    
+
     strcpy(showtimes[1].movieName, "Avengers: Endgame");
     strcpy(showtimes[1].time, "7:00 PM");
     memset(showtimes[1].seatMap, '.', ROWS * COLS);
     showtimes[1].bookingCount = 0;
     showtimes[1].totalRevenue = 0;
     showtimes[1].totalTicketsSold = 0;
-    
+
+    // Movie 2: Inception
     strcpy(showtimes[2].movieName, "Inception");
     strcpy(showtimes[2].time, "2:00 PM");
     memset(showtimes[2].seatMap, '.', ROWS * COLS);
     showtimes[2].bookingCount = 0;
     showtimes[2].totalRevenue = 0;
     showtimes[2].totalTicketsSold = 0;
-    
+
     strcpy(showtimes[3].movieName, "Inception");
     strcpy(showtimes[3].time, "6:30 PM");
     memset(showtimes[3].seatMap, '.', ROWS * COLS);
     showtimes[3].bookingCount = 0;
     showtimes[3].totalRevenue = 0;
     showtimes[3].totalTicketsSold = 0;
-    
+
+    // Movie 3: The Dark Knight
     strcpy(showtimes[4].movieName, "The Dark Knight");
     strcpy(showtimes[4].time, "4:30 PM");
     memset(showtimes[4].seatMap, '.', ROWS * COLS);
     showtimes[4].bookingCount = 0;
     showtimes[4].totalRevenue = 0;
     showtimes[4].totalTicketsSold = 0;
-    
+
     strcpy(showtimes[5].movieName, "The Dark Knight");
     strcpy(showtimes[5].time, "9:00 PM");
     memset(showtimes[5].seatMap, '.', ROWS * COLS);
     showtimes[5].bookingCount = 0;
     showtimes[5].totalRevenue = 0;
     showtimes[5].totalTicketsSold = 0;
-    
+
     showtimeCount = 6;
+}
+
+void displayMovieInfo() {
+    printf("Movies loaded successfully!\\n");
+}
+
+void displayAllShowtimes() {
+    printf("\\nAvailable Showtimes:\\n");
+
+    for(int i = 0; i < showtimeCount; i++) {
+        printf("%d. %s - %s\\n",
+               i + 1,
+               showtimes[i].movieName,
+               showtimes[i].time);
+    }
+}
+
+int main() {
+    initializeData();
+    displayMovieInfo();
+    displayAllShowtimes();
+
+    return 0;
 }
